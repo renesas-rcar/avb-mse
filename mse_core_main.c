renesas-rcar/avb-mse

commit dadb564c9b63521d10a85483d41645b3432d13ae

mse: core: code cleanup about packet buffer allocation
refs #117938

Signed-off-by: Mitsuhiro Kimura <mitsuhiro.kimura.kc@renesas.com>
Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>

mse_core_main.c
@@ -102,12 +102,16 @@
 /** @brief PTP table max */
 #define MSE_PTP_MAX                (10)
 
-#define MSE_DMA_MAX_PACKET         (128)
-#define MSE_DMA_MAX_PACKET_SIZE    (1526)
-#define MSE_DMA_MAX_RECEIVE_PACKET (64)
-#define MSE_DMA_MIN_RECEIVE_PACKET (60)
-
-#define MSE_VIDEO_START_CODE_LEN   (4)
+/** @brief packet buffer related definitions */
+#define MSE_PACKET_SIZE_MAX     (1526)
+#define MSE_TX_PACKET_NUM_MAX   (128)
+#define MSE_RX_PACKET_NUM_MAX   (128)
+#define MSE_TX_PACKET_NUM       (MSE_TX_PACKET_NUM_MAX)
+#define MSE_RX_PACKET_NUM       (64)
+#define MSE_TX_RING_SIZE        (MSE_TX_PACKET_NUM_MAX * 3)
+#define MSE_RX_RING_SIZE        (MSE_RX_PACKET_NUM_MAX * 2)
+#define MSE_CRF_TX_RING_SIZE    (MSE_TX_PACKET_NUM_MAX)
+#define MSE_CRF_RX_RING_SIZE    (MSE_RX_PACKET_NUM_MAX * 2)
 
 #define q_next(pos, max)        (((pos) + 1) % max)
 
@@ -1022,7 +1026,7 @@ static void mse_work_stream(struct work_struct *work)
 			/* request receive packet */
 			err = mse_packet_ctrl_receive_packet(
 				index_network,
-				MSE_DMA_MAX_RECEIVE_PACKET,
+				MSE_RX_PACKET_NUM,
 				dma, network);
 
 			if (err < 0) {
@@ -1649,8 +1653,17 @@ static int mse_initialize_crf_packetizer(struct mse_instance *instance)
 			goto error_set_cbs_param_fail;
 	}
 
+	if (instance->crf_type == MSE_CRF_TYPE_RX) {
+		ret = instance->network->set_streamid(
+					instance->crf_index_network,
+					instance->crf_net_config.streamid);
+		if (ret < 0)
+			goto error_set_streamid_fail;
+	}
+
 	return 0;
 
+error_set_streamid_fail:
 error_set_cbs_param_fail:
 error_calc_cbs_fail:
 error_set_audio_config_fail:
@@ -1676,7 +1689,7 @@ static void mse_release_crf_packetizer(struct mse_instance *instance)
 
 static bool check_packet_remain(struct mse_instance *instance)
 {
-	int wait_count = MSE_DMA_MAX_PACKET / 2;
+	int wait_count = MSE_TX_PACKET_NUM / 2;
 	int rem = mse_packet_ctrl_check_packet_remain(instance->packet_buffer);
 
 	return wait_count > rem;
@@ -2361,10 +2374,9 @@ static void mse_work_crf_receive(struct work_struct *work)
 {
 	struct mse_instance *instance;
 	struct mse_adapter *adapter;
-	int ret, err, count, i;
+	int err, count, i;
 	u64 ptimes[6];
 	unsigned long flags;
-	char *dev_name;
 
 	struct mse_packetizer_ops *crf = &mse_packetizer_crf_tstamp_audio_ops;
 
@@ -2372,28 +2384,6 @@ static void mse_work_crf_receive(struct work_struct *work)
 	adapter = instance->media;
 
 	mse_debug("START\n");
-
-	dev_name = adapter->config.network_device.device_name_rx_crf;
-	ret = instance->network->open(dev_name);
-	instance->crf_index_network = ret;
-	if (ret < 0)
-		mse_err("can not open %d\n", ret);
-
-	/* setup network */
-	instance->network->set_streamid(instance->crf_index_network,
-					instance->crf_net_config.streamid);
-
-	/* get packet memory */
-	instance->crf_packet_buffer = mse_packet_ctrl_alloc(
-		&mse->pdev->dev,
-		MSE_DMA_MAX_PACKET * 2,
-		MSE_DMA_MAX_PACKET_SIZE);
-
-	/* prepare for receive */
-	mse_packet_ctrl_receive_prepare_packet(
-		instance->crf_index_network,
-		instance->crf_packet_buffer,
-		instance->network);
 
 	do {
 		err = mse_packet_ctrl_receive_packet_crf(
@@ -2430,10 +2420,6 @@ static void mse_work_crf_receive(struct work_struct *work)
 
 	/* while state is RUNNABLE */
 	} while (mse_state_test(instance, MSE_STATE_RUNNABLE));
-
-	instance->network->release(instance->crf_index_network);
-	mse_packet_ctrl_free(instance->crf_packet_buffer);
-	instance->crf_packet_buffer = NULL;
 }
 
 static enum hrtimer_restart mse_crf_callback(struct hrtimer *arg)
@@ -3619,6 +3605,150 @@ static int check_mch_config(struct mse_instance *instance)
 	return 0; /* Valid */
 }
 
+/* free packet buffer */
+static void packet_buffer_free(struct mse_instance *instance)
+{
+	mse_packet_ctrl_free(instance->packet_buffer);
+	instance->packet_buffer = NULL;
+}
+
+/* allocate packet buffer */
+static int packet_buffer_alloc(struct mse_instance *instance)
+{
+	int ring_size;
+	struct mse_packet_ctrl *packet_buffer;
+
+	ring_size = instance->tx ? MSE_TX_RING_SIZE : MSE_RX_RING_SIZE;
+
+	packet_buffer = mse_packet_ctrl_alloc(&mse->pdev->dev,
+					      ring_size,
+					      MSE_PACKET_SIZE_MAX);
+
+	if (!packet_buffer)
+		return -ENOMEM;
+
+	instance->packet_buffer = packet_buffer;
+
+	return 0;
+}
+
+/* associate packet buffer with network adapter */
+static int packet_buffer_prepare(struct mse_instance *instance)
+{
+	if (instance->tx)
+		return mse_packet_ctrl_send_prepare_packet(
+						instance->index_network,
+						instance->packet_buffer,
+						instance->network);
+	else
+		return mse_packet_ctrl_receive_prepare_packet(
+						instance->index_network,
+						instance->packet_buffer,
+						instance->network);
+}
+
+static void crf_network_cleanup(struct mse_instance *instance)
+{
+	if (instance->crf_index_network < 0)
+		return;
+
+	instance->network->release(instance->crf_index_network);
+	instance->crf_index_network = MSE_INDEX_UNDEFINED;
+
+	mse_packet_ctrl_free(instance->crf_packet_buffer);
+	instance->crf_packet_buffer = NULL;
+}
+
+static int crf_tx_network_setup(struct mse_instance *instance)
+{
+	int ret;
+	int index_network;
+	struct mse_adapter_network_ops *network;
+	struct mse_packet_ctrl *packet_buffer;
+	char *dev_name;
+
+	network = instance->network;
+	dev_name =
+		instance->media->config.network_device.device_name_tx_crf;
+
+	index_network = network->open(dev_name);
+	if (index_network < 0)
+		return index_network;
+
+	/* allocate packet buffer */
+	packet_buffer = mse_packet_ctrl_alloc(&mse->pdev->dev,
+					      MSE_CRF_TX_RING_SIZE,
+					      MSE_PACKET_SIZE_MAX);
+
+	if (!packet_buffer) {
+		network->release(index_network);
+
+		return -ENOMEM;
+	}
+
+	/* associate crf packet buffer with network adapter */
+	ret = mse_packet_ctrl_send_prepare_packet(index_network,
+						  packet_buffer,
+						  instance->network);
+
+	if (ret) {
+		mse_packet_ctrl_free(packet_buffer);
+		network->release(index_network);
+
+		return ret;
+	}
+
+	instance->crf_index_network = index_network;
+	instance->crf_packet_buffer = packet_buffer;
+
+	return 0;
+}
+
+static int crf_rx_network_setup(struct mse_instance *instance)
+{
+	int ret;
+	int index_network;
+	struct mse_adapter_network_ops *network;
+	struct mse_packet_ctrl *packet_buffer;
+	char *dev_name;
+
+	network = instance->network;
+	dev_name =
+		instance->media->config.network_device.device_name_rx_crf;
+
+	index_network = network->open(dev_name);
+	if (index_network < 0)
+		return index_network;
+
+	/* allocate packet buffer */
+	packet_buffer = mse_packet_ctrl_alloc(&mse->pdev->dev,
+					      MSE_CRF_RX_RING_SIZE,
+					      MSE_PACKET_SIZE_MAX);
+
+	if (!packet_buffer) {
+		network->release(index_network);
+
+		return -ENOMEM;
+	}
+
+	/* associate crf packet buffer with network adapter */
+	ret = mse_packet_ctrl_receive_prepare_packet(index_network,
+						     packet_buffer,
+						     network);
+
+	if (ret) {
+		mse_packet_ctrl_free(packet_buffer);
+		network->release(index_network);
+
+		return ret;
+	}
+
+	instance->crf_index_network = index_network;
+	instance->crf_packet_buffer = packet_buffer;
+
+	return 0;
+}
+
 int mse_open(int index_media, bool tx)
 {
 	struct mse_instance *instance;
@@ -3859,71 +3989,43 @@ int mse_open(int index_media, bool tx)
 		}
 	}
 
-	/* get packet memory */
-	if (instance->tx)
-		instance->packet_buffer = mse_packet_ctrl_alloc(
-						&mse->pdev->dev,
-						MSE_DMA_MAX_PACKET * 3,
-						MSE_DMA_MAX_PACKET_SIZE);
-	else
-		instance->packet_buffer = mse_packet_ctrl_alloc(
-						&mse->pdev->dev,
-						MSE_DMA_MAX_PACKET * 2,
-						MSE_DMA_MAX_PACKET_SIZE);
+	/* allocate packet buffer */
+	err = packet_buffer_alloc(instance);
+	if (err)
+		goto error_packet_buffer_alloc;
 
-	if (instance->tx)
-		mse_packet_ctrl_send_prepare_packet(
-						instance->index_network,
-						instance->packet_buffer,
-						instance->network);
-	else
-		mse_packet_ctrl_receive_prepare_packet(
-						instance->index_network,
-						instance->packet_buffer,
-						instance->network);
+	/* associate packet buffer with network adapter */
+	err = packet_buffer_prepare(instance);
+	if (err)
+		goto error_packet_buffer_prepare;
 
 	/* send clock using CRF */
-	if (instance->crf_type == MSE_CRF_TYPE_TX) {
-		int ret;
+	if (instance->crf_type == MSE_CRF_TYPE_TX)
+		err = crf_tx_network_setup(instance);
 
-		dev_name = network_device->device_name_tx_crf;
-		ret = instance->network->open(dev_name);
-		if (ret < 0) {
-			err = -EINVAL;
+	/* receive clock using CRF */
+	if (instance->crf_type == MSE_CRF_TYPE_RX)
+		err = crf_rx_network_setup(instance);
 
-			goto error_cannot_open_network_device_for_crf;
-		}
-
-		instance->crf_index_network = ret;
-
-		/* get packet memory */
-		instance->crf_packet_buffer = mse_packet_ctrl_alloc(
-			&mse->pdev->dev,
-			MSE_DMA_MAX_PACKET,
-			MSE_DMA_MAX_PACKET_SIZE);
-		if (!instance->crf_packet_buffer) {
-			err = -EINVAL;
-
-			goto error_cannot_alloc_crf_packet_buffer;
-		}
-
-		/* prepare for send */
-		mse_packet_ctrl_send_prepare_packet(
-			instance->crf_index_network,
-			instance->crf_packet_buffer,
-			instance->network);
-	}
+	if (err)
+		goto error_cannot_crf_network_setup;
 
 	/* init paketizer */
-	instance->packetizer->init(instance->index_packetizer);
+	err = instance->packetizer->init(instance->index_packetizer);
+
+	if (err)
+		goto error_packetizer_init;
 
 	return index;
 
-error_cannot_alloc_crf_packet_buffer:
-	network->release(instance->crf_index_network);
+error_packetizer_init:
+	crf_network_cleanup(instance);
 
-error_cannot_open_network_device_for_crf:
-	mse_packet_ctrl_free(instance->packet_buffer);
+error_cannot_crf_network_setup:
+error_packet_buffer_prepare:
+	packet_buffer_free(instance);
+
+error_packet_buffer_alloc:
 	if (m_ops)
 		m_ops->close(instance->mch_dev_id);
 
@@ -4014,22 +4116,20 @@ int mse_close(int index)
 	if (IS_MSE_TYPE_AUDIO(adapter->type))
 		destroy_workqueue(instance->wq_crf_packet);
 
-	/* release network adapter */
-	instance->network->release(instance->index_network);
-	if (instance->crf_type == MSE_CRF_TYPE_TX)
-		instance->network->release(instance->crf_index_network);
-
 	/* release packetizer */
 	mse_packetizer_release(instance->packetizer_id,
 			       instance->index_packetizer);
+
+	/* release network adapter */
+	instance->network->release(instance->index_network);
+
+	/* free packet buffer */
+	packet_buffer_free(instance);
+
 	mse_release_crf_packetizer(instance);
 
-	/* free packet memory */
-	if (instance->packet_buffer)
-		mse_packet_ctrl_free(instance->packet_buffer);
-
-	if (instance->crf_packet_buffer)
-		mse_packet_ctrl_free(instance->crf_packet_buffer);
+	/* cleanup crf tx*/
+	crf_network_cleanup(instance);
 
 	err = mse_ptp_close(instance->ptp_index, instance->ptp_dev_id);
 	if (err < 0)

commit dbd367f007f56040928c7bb9bf05eed4c6300e4d

mse: core: Fix initialization of crf_index_network
refs #127463

Fixes: dadb564c9b63 "mse: core: code cleanup about packet buffer allocation"
Signed-off-by: Mitsuhiro Kimura <mitsuhiro.kimura.kc@renesas.com>
Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>

mse_core_main.c
@@ -4585,6 +4585,7 @@ int mse_close(int index)
 	instance->state = MSE_STATE_CLOSE;
 	instance->index_media = MSE_INDEX_UNDEFINED;
 	instance->index_network = MSE_INDEX_UNDEFINED;
+	instance->crf_index_network = MSE_INDEX_UNDEFINED;
 	instance->index_packetizer = MSE_INDEX_UNDEFINED;
 	instance->mch_index = MSE_INDEX_UNDEFINED;
 	instance->ptp_index = MSE_INDEX_UNDEFINED;
@@ -4938,6 +4939,7 @@ static int mse_probe(void)
 		mse->instance_table[i].state = MSE_STATE_CLOSE;
 		mse->instance_table[i].index_media = MSE_INDEX_UNDEFINED;
 		mse->instance_table[i].index_network = MSE_INDEX_UNDEFINED;
+		mse->instance_table[i].crf_index_network = MSE_INDEX_UNDEFINED;
 		mse->instance_table[i].index_packetizer = MSE_INDEX_UNDEFINED;
 		mse->instance_table[i].mch_index = MSE_INDEX_UNDEFINED;
 		mse->instance_table[i].ptp_index = MSE_INDEX_UNDEFINED;