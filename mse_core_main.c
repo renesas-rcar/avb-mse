renesas-rcar/avb-mse

commit 1d031494f3d2b9d9a900d790a8d911649f3aebb4

mse: core: init DMA even without CONFIG_OF set.
refs #123393

DMA allocation via dma_alloc_coherent fails without CONFIG_OF.
MSE does not require OF. Reason is that the used of_dma_configure
evaluates to a dummy routine without CONFIG_OF enabled. This leads to
dma_mask for device not initialized and resulting in no DMA allocation
possible. Solution provided with this patch is to set the mask manually
by dma_coerce_mask_and_coherent if no OF support is enabled.

Alternate solution could also be to replace of_dma_configure with
dma_coerce_mask_and_coherent.

Signed-off-by: Andreas Pape <apape@de.adit-jv.com>
Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>

mse_core_main.c
@@ -77,6 +77,7 @@
 #include <linux/delay.h>
 #include <linux/ptp_clock.h>
 #include <linux/list.h>
+#include <linux/dma-mapping.h>
 #include "avtp.h"
 #include "ravb_mse_kernel.h"
 #include "mse_packetizer.h"
@@ -3918,7 +3919,17 @@ static int mse_probe(void)
 	}
 
 	/* W/A for cannot using DMA APIs */
-	of_dma_configure(&mse->pdev->dev, NULL);
+	if (IS_ENABLED(CONFIG_OF))
+		of_dma_configure(&mse->pdev->dev, NULL);
+	else
+		/*
+		 * MSE has no dependency to OF, but w/o CONFIG_OF set the
+		 * above function does nothing while at least initializing
+		 * dma_mask, coherent_dma_mask is mandatory. Limitation to
+		 * 32bit is needed, as struct eavb_entryvec relies on 32bit
+		 * addresses.
+		 */
+		dma_coerce_mask_and_coherent(&mse->pdev->dev, DMA_BIT_MASK(32));
 
 #if defined(CONFIG_MSE_SYSFS)
 	/* create class */

commit 2a0da0ff9c0b821e16e3046c86b7a99367eb3574

mse: core: add check of return value on dma api
refs #145689

Fixes: 1d031494f3d2 ("mse: core: init DMA even without CONFIG_OF set.")
Signed-off-by: Andreas Pape <apape@de.adit-jv.com>
Signed-off-by: Andy Lowe <andy_lowe@mentor.com>
Signed-off-by: Masaru Nagai <masaru.nagai.vx@renesas.com>

mse_core_main.c
@@ -5012,9 +5012,9 @@ static int mse_probe(void)
 	}
 
 	/* W/A for cannot using DMA APIs */
-	if (IS_ENABLED(CONFIG_OF))
+	if (IS_ENABLED(CONFIG_OF)) {
 		of_dma_configure(&mse->pdev->dev, NULL);
-	else
+	} else {
 		/*
 		 * MSE has no dependency to OF, but w/o CONFIG_OF set the
 		 * above function does nothing while at least initializing
@@ -5022,7 +5022,13 @@ static int mse_probe(void)
 		 * 32bit is needed, as struct eavb_entryvec relies on 32bit
 		 * addresses.
 		 */
-		dma_coerce_mask_and_coherent(&mse->pdev->dev, DMA_BIT_MASK(32));
+		err = dma_coerce_mask_and_coherent(&mse->pdev->dev,
+						   DMA_BIT_MASK(32));
+		if (err) {
+			mse_err("Failed to configure DMA masks. ret=%d\n", err);
+			goto error;
+		}
+	}
 
 #if defined(CONFIG_MSE_SYSFS)
 	/* create class */